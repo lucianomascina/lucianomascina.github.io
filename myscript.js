127496fab7017d33459e6833ecdcb7a0badf616b
lucianomascina <lucianomascina@gmail.com>
2022-10-01 22:45:15 -0300

    se realizaron las funciones

---
+++ myscript.js
@@ -0,0 +1,26 @@
+
+document.getElementById("save").addEventListener("click", function(event){
+
+    event.preventDefault();
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+
+    if(nombre === null || nombre.trim() === "")
+        alert("debe ingresar un nombre");
+
+    if(apellido === null || apellido.trim() === "")
+        alert("debe ingresar un apellido");
+
+});
+
+document.getElementById("clean").addEventListener("click", function(){
+
+    document.getElementById("nombre").value = "";
+    document.getElementById("apellido").value = "";
+    document.getElementById("edad").value = "";
+    document.getElementById("empresa").value = "";
+    document.getElementById("masculino").checked = false;
+    document.getElementById("femenino").checked = false;
+    document.getElementById("otro").checked = false;
+
+});
