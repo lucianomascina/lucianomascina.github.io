6237836a0ffd9664230f2b24c28f5e62a5feff62
lucianomascina <lucianomascina@gmail.com>
2022-10-02 18:01:21 -0300

    se agregaron estilos

--- myscript.js
+++ myscript.js
@@ -1,16 +1,21 @@
+var nombre = document.getElementById("nombre").value;
+var apellido = document.getElementById("apellido").value;
+var edad = document.getElementById("edad").value;
+var empresa = document.getElementById("empresa").value;
 
 document.getElementById("save").addEventListener("click", function(event){
 
     event.preventDefault();
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
+    let edad = document.getElementById("edad").value; 
 
     if(nombre === null || nombre.trim() === "" || !isNaN(nombre))
         alert("debe ingresar un nombre");
-
     if(apellido === null || apellido.trim() === "" || !isNaN(apellido))
         alert("debe ingresar un apellido");
-
+    if(isNaN(edad))
+        alert("la edad no puede contener letras");
 });
 
 document.getElementById("clean").addEventListener("click", function(){
@@ -24,3 +29,4 @@ document.getElementById("clean").addEventListener("click", function(){
     document.getElementById("otro").checked = false;
 
 });
+
